1a6b38a401e6f0691667a03be0059cb8d2b0b8d5
josdmyer <josdmyer@gmail.com>
2020-10-01 18:34:22 -0500

    Move usage statment to top for ease
    Move usage statment to top for ease

--- backup.sh
+++ backup.sh
@@ -15,6 +15,16 @@
  default_src=/Users/x/Downloads/files/
  default_dest=/Volumes/My\ Passport/files/
 
+ usage()
+ {
+    echo "./backup              (backup default source to default destination)"
+    echo "./backup -s path      (backup specific source to default destination)"
+    echo "./backup -d path      (backup default source to specific destination)"
+    echo "./backup    path path (backup specific source to specific destination)"
+    press
+    clear
+ }
+
  # Function to prompt for key return until then wait
  press()
  {
@@ -44,16 +54,6 @@
  src_dest()
  {
  	/Users/x/Downloads/Software/rsync-3.1.1/rsync -a --info=progress2 "$1/" "$2/"
- }
-
- usage()
- {
- 	echo "./backup              (backup default source to default destination)"
- 	echo "./backup -s path      (backup specific source to default destination)"
- 	echo "./backup -d path      (backup default source to specific destination)"
- 	echo "./backup    path path (backup specific source to specific destination)"
- 	press
- 	clear
  }
 
  # ------------------------------ Main ---------------------------------------
