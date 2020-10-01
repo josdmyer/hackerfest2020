bf1f688c76d717138bd05dd2bdc7f487bffd4fc1
josdmyer <josdmyer@gmail.com>
2020-10-01 18:25:50 -0500

    Clean up and make simple
    Clean up and make simple

--- stats.sh
+++ stats.sh
@@ -1,48 +1,31 @@
 #! /bin/bash
 
-  # --------------------------------------------------------------------------
-  # Name:         x
-  # Date:         Oct 1, 2020
-  # File Name:    stats.sh
-  # Descriptoin:  A bash script to give size of each format type in Books 
-  #				directory to let me know how big my book directory is getting 
-  # -------------------------------------------------------------------------
-  #! /bin/bash
+ # --------------------------------------------------------------------------
+ # Name:         x
+ # Date:         Oct 1, 2020
+ # File Name:    stats.sh
+ # Descriptoin:  A bash script to give size of each format type in Books 
+ #				directory to let me know how big my book directory is getting 
+ # ---------------------------------------------------------------------------
 
-  files1=$(find /Users/x/Documents/Books/Books/ -type f -iname '*.epub' | wc -l)
+ files1=$(find /Users/x/Documents/Books/Books/ -type f -iname '*.epub' | wc -l)
 
-  files2=$(find /Users/x/Documents/Books/Books/ -type f -iname '*.pdf' | wc -l)
+ files4=$(find /Users/x/Documents/Books/Books/ -type f | wc -l)
 
-  files3=$(find /Users/x/Documents/Books/Books/ -type f -iname '*.mobi' | wc -l)
+ clear
 
-  files4=$(find /Users/x/Documents/Books/Books/ -type f | wc -l)
+ cd /Users/x/Documents/Books/Books/
 
-  clear
+ echo -n "Epub:  "
+ find . -type f -iname '*.epub' -exec du -ch {} + | grep total$ 
 
-  cd /Users/x/Documents/Books/Books/
+ echo "Number of files:  $files1"
+ echo
 
-  echo -n "Epub:  "
-  find . -type f -iname '*.epub' -exec du -ch {} + | grep total$ 
+ echo -n "Total:  "
+ du -sh /Users/x/Documents/Books/Books/
 
-  echo "Number of files:  $files1"
-  echo
-
-  echo -n "Pdf:  "
-  find /Users/x/Documents/Books/Books/ -type f -iname '*.pdf' -exec du -ch {} + | grep total$
-
-  echo "Number of files:  $files2"
-  echo
-
-  echo -n "Mobi:  "
-  find /Users/x/Documents/Books/Books/ -type f -iname '*.mobi' -exec du -ch {} + | grep total$
-
-  echo "Number of files:  $files3"
-  echo
-
-  echo -n "Total:  "
-  du -sh /Users/x/Documents/Books/Books/
-
-  echo "Number of files:  $files4"
-  echo
-  cd
-  #--------------------------------- End of file --------------------------------
+ echo "Number of files:  $files4"
+ echo
+ cd
+ #--------------------------------- End of file -----------------------------
